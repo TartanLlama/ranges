eddfc99929b24f72b1a9f4181679975fc71578a4
Sy Brand <tartanllama@gmail.com>
2021-04-07 15:50:45 +0100

    Add enumerate implementation

---
+++ tests/enumerate.cpp
@@ -0,0 +1,54 @@
+#include "tl/enumerate.hpp"
+#include <catch2/catch.hpp>
+#include <iostream>
+#include <vector>
+
+TEST_CASE("basic vector") {
+  std::vector a{1, 2, 3};
+  int i = 0;
+  for (auto&& [index, item] : a | tl::views::enumerate) {
+    REQUIRE(index == i);
+    REQUIRE(item == i + 1);
+    ++i;
+  }
+}
+
+TEST_CASE("basic const vector") {
+  const std::vector a{1, 2, 3};
+  int i = 0;
+  for (auto&& [index, item] : a | tl::views::enumerate) {
+    REQUIRE(index == i);
+    REQUIRE(item == i + 1);
+    ++i;
+  }
+}
+
+TEST_CASE("transform") {
+  const std::vector a{1, 2, 3};
+  int i = 0;
+  for (auto&& [index, item] :
+       (a | std::views::transform([](auto i) { return i - 1; }) |
+        tl::views::enumerate)) {
+    REQUIRE(index == i);
+    REQUIRE(item == i);
+    ++i;
+  }
+}
+
+TEST_CASE("modify vector") {
+  std::vector a{1, 2, 3};
+  int i = 0;
+  for (auto&& [index, item] : a | tl::views::enumerate) {
+    REQUIRE(index == i);
+    REQUIRE(item == i + 1);
+    item--;
+    ++i;
+  }
+  i = 0;
+  for (auto&& [index, item] : a | tl::views::enumerate) {
+    REQUIRE(index == i);
+    REQUIRE(item == i);
+    ++i;
+  }
+  REQUIRE(a == std::vector{0, 1, 2});
+}
