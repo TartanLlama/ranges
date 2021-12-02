be5bd2991283cb36042e3c6a7c735265ca55775b
Casey Carter <Casey@Carter.net>
2021-12-01 21:38:49 -0800

    Use two iterators (instead of N) to implement adjacent_view
    `read` is still O(N), of course, but iterators are otherwise smaller and cheaper. Drive-by: merge pairs of `end()` and `end() const` overloads replacing concept overloading with `if constexpr` dispatch.

--- tests/adjacent.cpp
+++ tests/adjacent.cpp
@@ -9,10 +9,14 @@ TEST_CASE("adjacent") {
    std::vector<std::tuple<int, int, int>> results{
       {0,1,2},
       {1,2,3},
-      {2,3,4}
+      {2,3,4},
+      {3,4,5},
+      {4,5,6}
    };
 
-   for (auto [a,b] : tl::views::zip(tl::views::adjacent<3>(a), results)) {
+   REQUIRE(tl::views::adjacent<3>(a).size() == 5);
+
+   for (auto const& [a,b] : tl::views::zip(tl::views::adjacent<3>(a), results)) {
       REQUIRE(a == b);
    }
 }
